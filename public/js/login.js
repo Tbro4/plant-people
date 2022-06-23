b5e5d7fb10dbd848d7af286de87bcd95ab01165f
Tyler Brooks <thetylerbrooks@gmail.com>
2022-06-22 18:44:50 -0600

    Front end layouts created (need to change to .handlebars after done testing)

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#inputEmail").value.trim();
+  const password = document.querySelector("#inputPassword").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#inputUsername").value.trim();
+  const email = document.querySelector("#inputEmail").value.trim();
+  const password = document.querySelector("#inputPassword").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
